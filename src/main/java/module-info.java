719b6f05fadfe5c3b9e77d2d5b1c5017b9f5cfa3
Canary-Prism <canaryprsn@gmail.com>
2024-11-14 02:38:10 +0800

    add exported apis bc apparently that's imporpant :p

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module canaryprism.slavacord {
     requires org.javacord.core;
     requires org.apache.logging.log4j;
+
+    exports canaryprism.slavacord;
+    exports canaryprism.slavacord.annotations;
+    exports canaryprism.slavacord.exceptions;
 }
