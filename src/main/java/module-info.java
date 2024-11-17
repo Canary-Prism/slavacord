bb6070d10994fea480db05914a3ccac98ace1f01
Canary-Prism <canaryprsn@gmail.com>
2024-11-17 16:53:24 +0800

    uhh i guess make this depend on api now?

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module canaryprism.slavacord {
-    requires transitive org.javacord.core;
+    requires transitive org.javacord.api;
     requires org.apache.logging.log4j;
 
     exports canaryprism.slavacord;
