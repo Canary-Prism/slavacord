b13a50466dc5d1b885ef2eeaca09cf866c78032b
Canary-Prism <canaryprsn@gmail.com>
2024-11-19 03:04:27 +0800

    i guess i'll add this javadoc here bc i'm nice

--- src/main/java/canaryprism/slavacord/data/LocalizationData.java
+++ src/main/java/canaryprism/slavacord/data/LocalizationData.java
@@ -4,5 +4,8 @@ import java.util.Map;
 
 import org.javacord.api.interaction.DiscordLocale;
 
+/**
+ * combined name and description localizations for use by commands and options
+ */
 public record LocalizationData(Map<DiscordLocale, String> names, Map<DiscordLocale, String> descriptions) {
 }
