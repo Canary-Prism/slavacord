12b4ef15914b0f149b91f0ffd68a16104ea88446
Canary-Prism <canaryprsn@gmail.com>
2024-11-21 01:56:53 +0800

    Javadocs :3

--- src/main/java/canaryprism/slavacord/annotations/optionbounds/ChannelTypeBounds.java
+++ src/main/java/canaryprism/slavacord/annotations/optionbounds/ChannelTypeBounds.java
@@ -5,8 +5,18 @@ import java.lang.annotation.Target;
 
 import org.javacord.api.entity.channel.ChannelType;
 
+/**
+ * <p>the bounds of a CHANNEL type option.</p>
+ * <p>when used the user may only select a channel from one of these types.</p>
+ * <p>the declared parameter's type must be able to hold all of these channel types</p>
+ */
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 @Target({ java.lang.annotation.ElementType.PARAMETER })
 public @interface ChannelTypeBounds {
+
+    /**
+     * the channel types that the user may select from
+     * @return an array of channel types that the user may select from
+     */
     ChannelType[] value();
 }
--- src/main/java/canaryprism/slavacord/annotations/optionbounds/LongBounds.java
+++ src/main/java/canaryprism/slavacord/annotations/optionbounds/LongBounds.java
@@ -3,10 +3,22 @@ package canaryprism.slavacord.annotations.optionbounds;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * <p>the bounds of a LONG option.</p>
+ */
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 @Target({ java.lang.annotation.ElementType.PARAMETER })
 public @interface LongBounds {
+
+    /**
+     * the minimum value that the user may enter, less than or equal to {@link #max}
+     * @return the minimum value that the user may enter
+     */
     long min() default Long.MIN_VALUE;
 
+    /**
+     * the maximum value that the user may enter, greater than or equal to {@link #min}
+     * @return the maximum value that the user may enter
+     */
     long max() default Long.MAX_VALUE;
 }
