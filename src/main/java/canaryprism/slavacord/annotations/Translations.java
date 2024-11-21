878effa017428229dd4cce0a2d58f76d48b2d394
Canary-Prism <canaryprsn@gmail.com>
2024-11-21 23:12:10 +0800

    add javadocs for Translations

--- src/main/java/canaryprism/slavacord/annotations/Translations.java
+++ src/main/java/canaryprism/slavacord/annotations/Translations.java
@@ -4,8 +4,16 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * container annotation for {@link Trans} annotations
+ */
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE})
 public @interface Translations {
+
+    /**
+     * array of {@link Trans} translation values. prefer repeating the annotation if possible
+     * @return an array of Trans translations
+     */
     Trans[] value();
 }
