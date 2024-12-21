e503f26d7c0127a129083f78446d10f37acb703f
Canary-Prism <canaryprsn@gmail.com>
2024-12-21 20:43:47 +0900

    fix javadoc

--- src/main/java/canaryprism/slavacord/annotations/optionbounds/DoubleBounds.java
+++ src/main/java/canaryprism/slavacord/annotations/optionbounds/DoubleBounds.java
@@ -1,5 +1,8 @@
 package canaryprism.slavacord.annotations.optionbounds;
 
+import canaryprism.discordbridge.api.interaction.slash.SlashCommandOption;
+import canaryprism.discordbridge.api.interaction.slash.SlashCommandOptionType;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -12,7 +15,7 @@ public @interface DoubleBounds {
 
     /**
      * <p>the minimum value that the user may enter less than or equal to {@link #max}</p>
-     * <p>this value must not be smaller than -2^53 as specified by {@link org.javacord.api.interaction.SlashCommandOptionType#DECIMAL}</p>
+     * <p>this value must not be smaller than -2^53 as specified by {@link SlashCommandOption#MIN_NUMBER}</p>
      * <p>it can, however, be {@link Double#NEGATIVE_INFINITY} as this library ignores this value</p>
      *
      * @return the minimum value that the user may enter
@@ -21,7 +24,7 @@ public @interface DoubleBounds {
 
     /**
      * <p>the maximum value that the user may enter greater than or equal to {@link #min}</p>
-     * <p>this value must not be greater than 2^53 as specified by {@link org.javacord.api.interaction.SlashCommandOptionType#DECIMAL}</p>
+     * <p>this value must not be greater than 2^53 as specified by {@link SlashCommandOption#MAX_NUMBER}</p>
      * <p>it can, however, be {@link Double#POSITIVE_INFINITY} as this library ignores this value</p>
      *
      * @return the maximum value that the user may enter
--- src/main/java/canaryprism/slavacord/annotations/optionbounds/LongBounds.java
+++ src/main/java/canaryprism/slavacord/annotations/optionbounds/LongBounds.java
@@ -1,5 +1,7 @@
 package canaryprism.slavacord.annotations.optionbounds;
 
+import canaryprism.discordbridge.api.interaction.slash.SlashCommandOption;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -12,7 +14,7 @@ public @interface LongBounds {
 
     /**
      * <p>the minimum value that the user may enter less than or equal to {@link #max}</p>
-     * <p>this value must not be smaller than -2^53 as specified by {@link org.javacord.api.interaction.SlashCommandOptionType#LONG}</p>
+     * <p>this value must not be smaller than -2^53 as specified by {@link SlashCommandOption#MIN_NUMBER}</p>
      * <p>it can, however, be {@link Long#MIN_VALUE} as this library ignores this value</p>
      *
      * @return the minimum value that the user may enter
@@ -21,7 +23,7 @@ public @interface LongBounds {
 
     /**
      * <p>the maximum value that the user may enter greater than or equal to {@link #min}</p>
-     * <p>this value must not be greater than 2^53 as specified by {@link org.javacord.api.interaction.SlashCommandOptionType#LONG}</p>
+     * <p>this value must not be greater than 2^53 as specified by {@link SlashCommandOption#MAX_NUMBER}</p>
      * <p>it can, however, be {@link Long#MAX_VALUE} as this library ignores this value</p>
      *
      * @return the maximum value that the user may enter
