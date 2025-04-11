f162a5e905974bec0994d772c106a04f71cb7c06
Canary-Prism <canaryprsn@gmail.com>
2025-04-11 23:36:38 +0800

    upgrade discord-bridge version to 6.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.github.canary-prism"
-version = "7.0.7"
+version = "7.0.8"
 description = "A library addon for Discord Bridge that adds a more convenient syntax for creating slash commands."
 
 repositories {
