1c47a377156904636b3c2a276cff077c1610bdc5
Canary-Prism <canaryprsn@gmail.com>
2025-08-07 16:59:59 +0800

    SPIs hehe

---
+++ slavacord-processor/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+import canaryprism.slavacord.annotations.processor.*;
+
+module canaryprism.slavacord.processor {
+    uses canaryprism.discordbridge.api.DiscordBridge;
+    requires java.compiler;
+    requires canaryprism.slavacord;
+    requires canaryprism.discordbridge.api;
+    requires org.apache.commons.lang3;
+
+    provides javax.annotation.processing.Processor with
+            CreateAnnotationProcessor,
+            CommandAnnotationProcessor,
+            AsyncAnnotationProcessor,
+            ReturnsResponseAnnotationProcessor,
+            OptionAnnotationProcessor,
+            AutocompletesAnnotationProcessor;
+}
