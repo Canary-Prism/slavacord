6604db7effa90ada8d2a020808f0c895421ef7a2
Canary-Prism <canaryprsn@gmail.com>
2025-08-08 23:09:17 +0800

    check that @Autocompletes points to valid autocompleter method

--- slavacord-processor/src/main/java/canaryprism/slavacord/annotations/processor/AutocompletesAnnotationProcessor.java
+++ slavacord-processor/src/main/java/canaryprism/slavacord/annotations/processor/AutocompletesAnnotationProcessor.java
@@ -1,15 +1,17 @@
 package canaryprism.slavacord.annotations.processor;
 
 
-import canaryprism.slavacord.autocomplete.annotations.Autocompleter;
+import canaryprism.slavacord.annotations.Option;
 import canaryprism.slavacord.autocomplete.annotations.Autocompletes;
 
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.*;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
 import javax.tools.Diagnostic;
-import java.util.stream.Collectors;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_17)
 @SupportedAnnotationTypes({
@@ -23,6 +25,9 @@ public final class AutocompletesAnnotationProcessor extends AbstractProcessor {
                     element, annotation_mirror);
             return;
         }
-
+        // only thing we can really do here is check @Option is present since that's where @Autocompletes is actually processed
+        if (parameter.getAnnotation(Option.class) == null)
+            message(Diagnostic.Kind.ERROR, "@%s cannot be used without @%s"
+                    .formatted(Autocompletes.class.getSimpleName(), Option.class.getSimpleName()), element, annotation_mirror);
     }
 }
