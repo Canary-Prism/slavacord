3b6f7c99ac2c247bb4be21b6364ace28700543c2
Canary-Prism <canaryprsn@gmail.com>
2025-08-10 14:47:01 +0800

    check @CommandGroup type is within either a Commands class or @CommandGroup type and check nesting depth

--- slavacord-processor/src/main/java/canaryprism/slavacord/annotations/processor/CommandGroupAnnotationProcessor.java
+++ slavacord-processor/src/main/java/canaryprism/slavacord/annotations/processor/CommandGroupAnnotationProcessor.java
@@ -1,7 +1,10 @@
 package canaryprism.slavacord.annotations.processor;
 
+import canaryprism.slavacord.Commands;
 import canaryprism.slavacord.annotations.Command;
 import canaryprism.slavacord.annotations.CommandGroup;
+import canaryprism.slavacord.annotations.CreateGlobal;
+import canaryprism.slavacord.annotations.CreateServer;
 
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
@@ -30,6 +33,39 @@ public final class CommandGroupAnnotationProcessor extends AbstractProcessor {
                 message(Diagnostic.Kind.ERROR, "no-args constructor required for @%s containing nonstatic commands"
                         .formatted(CommandGroup.class.getSimpleName()), type, annotation_mirror);
             }
+        }
+
+        var depth = 0;
+
+        if (type.getEnclosingElement() instanceof TypeElement enclosing) {
+            while (true) {
+                if (enclosing.getAnnotation(CommandGroup.class) != null) {
+                    depth++;
+                    if (depth == 2) {
+                        message(Diagnostic.Kind.ERROR, "@%s may not be nested more than 2 levels"
+                                .formatted(CommandGroup.class.getSimpleName()), type, annotation_mirror);
+                    }
+                    if (enclosing.getEnclosingElement() instanceof TypeElement e) {
+                        enclosing = e;
+                        continue;
+                    }
+                } else if (enclosing.getInterfaces().contains(getTypeMirror(Commands.class))) {
+                    if (enclosing.getAnnotation(CreateGlobal.class) == null && enclosing.getAnnotation(CreateServer.class) == null)
+                        message(Diagnostic.Kind.ERROR,
+                                "%s type must be annotated with @%s or @%s"
+                                        .formatted(Commands.class.getSimpleName(), CreateGlobal.class.getSimpleName(), CreateServer.class.getSimpleName()),
+                                enclosing);
+                } else {
+                    message(Diagnostic.Kind.ERROR,
+                            "@%s type not nested in %s class or command group"
+                                    .formatted(CommandGroup.class.getSimpleName(), Commands.class.getSimpleName())
+                            , type, annotation_mirror);
+                }
+                break;
+            }
+        } else {
+            message(Diagnostic.Kind.ERROR, "@%s type must be nested in another type"
+                    .formatted(CommandGroup.class.getSimpleName()), type, annotation_mirror);
         }
     }
 
